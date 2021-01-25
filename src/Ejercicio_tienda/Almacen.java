7b332ef3004c677daafc6df66bc7f87d4cdc036a
Marcos Hernández Aragonés <marcoshernandezaragones@gmail.com>
2021-01-25 16:13:38 +0100

    Mercadoña

---
+++ src/Ejercicio_tienda/Almacen.java
@@ -0,0 +1,5 @@
+package Ejercicio_tienda;
+
+public class Almacen {
+
+}
--- src/ejercicio_Mercadoña/Perecedero.java
+++ src/ejercicio_Mercadoña/Perecedero.java
@@ -2,6 +2,13 @@ package ejercicio_Mercadoña;
 
 public final class Perecedero extends Producto{
 	
+	private int fecha_caducidad;
+
+	public Perecedero(String nombre, String descripcion, String codigo, int fecha_caducidad) {
+		super(nombre, descripcion, codigo);
+		this.fecha_caducidad = fecha_caducidad;
+	}
+	
 	
 	
 }
